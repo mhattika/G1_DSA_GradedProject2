507d992f1d8e82fc751f23afdb52cdf1e0068927
mhattika <75724296+mhattika@users.noreply.github.com>
2022-11-13 09:03:14 +0530

    Committed code for skyscraper problem1
    Committed code for skyscraper problem1

--- G1_DSA_GradedProject2/src/com/gl/dsa/skyscraper/problem1/MainDriver.java
+++ G1_DSA_GradedProject2/src/com/gl/dsa/skyscraper/problem1/MainDriver.java
@@ -1,10 +1,25 @@
 package com.gl.dsa.skyscraper.problem1;
 
+import java.util.Scanner;
+
 public class MainDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter the total no of floors in the building : ");
 
+		try (Scanner sc = new Scanner(System.in)) {
+			int numberOfFloors = sc.nextInt();
+			int[] listOfFloors = new int[numberOfFloors];
+			for (int i = 0; i < numberOfFloors; i++) {
+				System.out.println("Enter the floor size given on day : " + (i + 1));
+				int floorSize = sc.nextInt();
+				listOfFloors[i] = floorSize;
+			}
+
+			System.out.println("\n\nThe order of construction is as follows ");
+			System.out.println();
+			Skyscraper.calculateFloors(listOfFloors);
+		}
 	}
 
 }
